4fee270b04c96783c72840d8b2f2e4f530b2416a
Rami Bokhari <Rbokhari@mbsc.edu.sa>
2019-10-28 09:55:03 +0300

    Add react-router and implement simple page navigation

---
+++ src/components/AddPoll.js
@@ -0,0 +1,13 @@
+import React , { Component } from 'react'
+
+class AddPoll extends Component{
+    render(){
+        return(
+            <div>
+                Add
+            </div>
+        )
+    }
+}
+
+export default AddPoll
