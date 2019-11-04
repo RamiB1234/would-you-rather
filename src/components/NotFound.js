04b89962c23c9616ba0a27da77813c1e8299c03b
Rami Bokhari <rbisthebest@hotmail.com>
2019-11-04 13:58:49 +0300

    Modified question not found error message

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function NotFound() {
   return (
     <div className="text-center">
-      <h2>404 Ops, we couldn't find your question!</h2>
+      <h2>404 Error<br/>Ops, we couldn't find your question!</h2>
     </div>
   );
 }
